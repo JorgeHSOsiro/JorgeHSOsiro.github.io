814cd17216e43a3ef0770c25f3033a7b9cb01932
jorgehsosiro <jojimurakabe@gmail.com>
2021-05-03 11:31:52 -0300

    add project

--- src/page/Projects/index.js
+++ src/page/Projects/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import ProjectModal from '../../components/ProjectModal';
 import catz from '../../assets/catz-factz.png';
-// import cards from '../../assets/movie-cards-crud.png';
-import todo from '../../assets/to-do-list.png'
+import todo from '../../assets/to-do-list.png';
+import trybeer from '../../assets/trybeer_v2.png';
 import styles from './projects.module.css';
 
 export default function Projects() {
@@ -11,8 +11,27 @@ export default function Projects() {
     <div className={styles.mainProjContainer}>
       <h2>Projetos</h2>
       <div className={styles.projectContainer}>
-        <ProjectModal title="Catz Facts Project" techs="HTML | CSS" src={catz} gitlink="https://github.com/JorgeHSOsiro/CatzFacts" vid="https://www.youtube.com/embed/cr9LqPl3u8A" />
-        <ProjectModal title="To Do List" techs=" Javascript | HTML | CSS" src={todo} gitlink="https://github.com/JorgeHSOsiro/TodoList" vid="https://www.youtube.com/embed/1Xu1zXdoGQE" />
+        <ProjectModal
+          title="Catz Facts Project"
+          techs="HTML | CSS"
+          src={catz}
+          gitlink="https://github.com/JorgeHSOsiro/CatzFacts"
+          vid="https://www.youtube.com/embed/cr9LqPl3u8A"
+        />
+        <ProjectModal
+          title="To Do List"
+          techs=" Javascript | HTML | CSS"
+          src={todo}
+          gitlink="https://github.com/JorgeHSOsiro/TodoList"
+          vid="https://www.youtube.com/embed/1Xu1zXdoGQE"
+        />
+        <ProjectModal
+          title="Trybeer V2"
+          techs=" Javascript | React | Node | Sequelize | MongoDB"
+          src={trybeer}
+          gitlink="https://github.com/JorgeHSOsiro/trybeer-v2"
+          vid="https://www.youtube.com/embed/hO06MVS8V_w"
+        />
       </div>
     </div>
   );
